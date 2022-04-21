a2acf9824efe6bafafe7e297e91b5f4425a9c92d
Josher Ramírez <ramirezjosher@gmail.com>
2022-04-21 01:05:30 -0600

    tarea 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Josher Ramirez Montoya. 
+          B96368!
         </p>
         <a
           className="App-link"
